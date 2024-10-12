08ded4e445b0d52a5ae57e0a54dbf058f09a7a09
PurpleCre <pgonese6@gmail.com>
2024-10-12 19:56:53 +0200

    contacts: part two complete

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 const routes = require("express").Router();
-const routeController = require("../controllers/routes")
- 
-routes.get('/', routeController.panasheRoute);
-routes.get('/marjorie', routeController.marjorieRoute);
-routes.get('/primrose', routeController.primroseRoute);
-routes.get('/gamu', routeController.gamuRoute);
+// const routeController = require("../controllers/routes")
+
+routes.use('/', require('./swagger'));
+
+routes.get('/', (req, res) => {
+    // swagger.tags=['Hello World']
+    res.send('Hello World');
+})
+// routes.get('/', routeController.panasheRoute);
+// routes.get('/marjorie', routeController.marjorieRoute);
+// routes.get('/primrose', routeController.primroseRoute);
+// routes.get('/gamu', routeController.gamuRoute);
 
 routes.use('/contacts', require('./contacts'))
 
--- server.js
+++ server.js
@@ -2,10 +2,20 @@
 const express = require('express');
 const app = express();
 const mongodb = require("./db/connect");
-
+const bodyParser = require("body-parser");
 
 const port = 3000;
  
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+})
 app.use("/", require("./routes"));
 
 mongodb.initDb((err) => {
