f2093bca5558ee6fb284dd4e1c0d59e394b5bb72
PurpleCre <pgonese6@gmail.com>
2024-10-15 17:54:28 +0200

    contacts2: routes update

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,7 +2,7 @@ const mongo = require("../db/connect");
 const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
-        // swagger.tags=['Contacts']
+    //#swagger.tags=['Contacts']
     const result = await mongo.getDatabase().db().collection("contacts").find();
     result.toArray().then((contacts) => {
         res.setHeader("Content-Type", "application/json");
@@ -11,7 +11,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-        // swagger.tags=['Contacts']
+    //#swagger.tags=['Contacts']
     const contactId = new ObjectId(req.params.id);
     const result = await mongo.getDatabase().db().collection("contacts").find({ _id: contactId});
     result.toArray().then((contacts) => {
@@ -21,7 +21,7 @@ const getSingle = async (req, res) => {
 }
 
 const createContact = async(req, res) => {
-        // swagger.tags=['Contacts']
+    //#swagger.tags=['Contacts']
     const contact = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -39,7 +39,7 @@ const createContact = async(req, res) => {
 }
 
 const updateContact = async(req, res) => {
-        // swagger.tags=['Contacts']
+    //#swagger.tags=['Contacts']
     const contactId = new ObjectId(req.params.id);
     const contact = {
         firstName: req.body.firstName,
@@ -58,7 +58,7 @@ const updateContact = async(req, res) => {
 }
 
 const deleteContact = async(req, res) => {
-        // swagger.tags=['Contacts']
+    //#swagger.tags=['Contacts']
     const contactId = new ObjectId(req.params.id);
     const response = await mongo.getDatabase().db().collection("contacts").deleteOne({ _id: contactId});
 
